f447266b16580cce0d724c8e87514fd2bf7bf573
kawaii-flesh <47851113+kawaii-flesh@users.noreply.github.com>
2020-07-22 08:44:39 +0300

    Update npp.py
    Switched to sha256

--- npp.py
+++ npp.py
@@ -2,33 +2,33 @@ import sys
 import random
 import hashlib
 
-#Calculate SHA1 and get file size
+#Calculate SHA256 and get file size
 def Pack(fname):
-	fin = open(fname,"rb")
-	buff=fin.read()
-	size=len(buff)
-	MSN = hashlib.sha1(buff).hexdigest()
+	fin = open(fname, "rb")
+	buff = fin.read()
+	size = len(buff)
+	MSN = hashlib.sha256(buff).hexdigest()
 	fin.close()
 	fout = open(fname+".np","w")
 	fout.write(str(size)+"\n"+str(MSN))
 	fout.close()
 
-#Create random file and calculate SHA1 if equally write data
+#Create random file and calculate SHA256 if equally write data
 def UnPack(fname):
-	fin = open(fname,"r")
+	fin = open(fname, "r")
 	size = int(fin.readline())
-	MSN=fin.readline()
-	MSN1=0
+	MSN = fin.readline()
+	MSN1 = 0
 	fin.close()
-	while(MSN1!=MSN):
+	while(MSN1 != MSN):
 		buff = []
-		MSN1=r=0
-		while(r<size):
-			buff.append(chr(random.randint(0,255)))
-			r+=1
+		MSN1 = r = 0
+		while(r < size):
+			buff.append(chr(random.randint(0, 255)))
+			r += 1
 		buff = str(''.join(buff))
 		buff = buff.encode()
-		MSN1 = hashlib.sha1(buff).hexdigest()
-	fout=open(fname+".up","w")
+		MSN1 = hashlib.sha256(buff).hexdigest()
+	fout=open(fname+".up", "w")
 	fout.write(buff.decode())
 	fout.close() 
